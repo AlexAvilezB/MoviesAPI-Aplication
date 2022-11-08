9593e627e3e93a82dbde9949f62fc52a83cc0f89
Alejandro Avilez <aleantonio656@gmail.com>
2022-11-08 17:33:06 -0600

    Issues correction

--- app/src/main/java/com/example/moviesapi/TvShowAdapter.kt
+++ app/src/main/java/com/example/moviesapi/TvShowAdapter.kt
@@ -2,7 +2,6 @@ package com.example.moviesapi
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import coil.load
 import androidx.recyclerview.widget.RecyclerView
 
 class TvShowAdapter(private val dataSet: Array<TVShow>) : RecyclerView.Adapter<TvShowViewHolder>() {
