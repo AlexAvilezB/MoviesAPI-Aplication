a6864f67a841c93b10ed86710c3e0e48cf96ca65
Alejandro Avilez <aleantonio656@gmail.com>
2022-11-07 18:28:07 -0600

    Revert "Merge branch 'Desgin' into Develop"
    This reverts commit a328ebeb7947e0d2a626ba2609e89195f837199a, reversing
    changes made to 2e1191004161b17d0f9165528b6b85a53bbede3f.

--- app/src/main/java/com/example/moviesapi/MainActivity.kt
+++ app/src/main/java/com/example/moviesapi/MainActivity.kt
@@ -2,44 +2,10 @@ package com.example.moviesapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-
-        Api.retrofitService.getTvShows().enqueue(
-            object: Callback<List<TvShow>> {
-                override fun onResponse(call: Call<List<TvShow>>, response: Response<List<TvShow>>) {
-                    Log.i("Retrofit", response.body().toString())
-
-                    response.body()?.let {
-                        configureTvShowsList(it)
-                    }
-                }
-
-                override fun onFailure(call: Call<List<TvShow>>, t: Throwable) {
-                    Log.i("Retrofit", t.message.toString())
-                }
-            }
-        )
-    }
-
-    fun configureTvShowsList(data: List<TvShow>) {
-        val adapter = TvShowAdapter(dataSet = data.toTypedArray())
-
-        val recyclerView: RecyclerView = findViewById(R.id.tvShows_recycler_view)
-
-        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/moviesapi/TvShow.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviesapi
-
-import com.squareup.moshi.Json
-
-data class TvShow(
-    val id: Int,
-    val name: String,
-    val first_air_date: String
-)
