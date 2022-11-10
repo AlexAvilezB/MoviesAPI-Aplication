cf3784846ae18b7055317c0a7b08282a4428ba8a
Alejandro Avilez <aleantonio656@gmail.com>
2022-11-10 17:55:00 -0600

    Revert "Revert "ID Api?""
    This reverts commit 86aae2713c576093cfefbc2e0fcaa6cf2debb2c8.

--- app/src/main/java/com/example/moviesapi/TvShowViewHolder.kt
+++ app/src/main/java/com/example/moviesapi/TvShowViewHolder.kt
@@ -10,9 +10,9 @@ class TvShowViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
     val nameTextView: TextView = itemView.findViewById(R.id.name_textView)
     val posterPathImageView: ImageView = itemView.findViewById(R.id.tvShowImage)
 
-
     fun setTVShow(tvShow:TVShow){
         Glide.with(posterPathImageView.context).load("https://image.tmdb.org/t/p/w500" + tvShow.poster_path).into(posterPathImageView)
 
     }
+
 }
